1dab3f23fc6a3316f4fde545c9865343d0f44145
Yash <60144007+yash26bit@users.noreply.github.com>
2022-09-09 15:58:18 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+setInterval(() => {
+    d = new Date();
+    htime = d.getHours();
+    mtime = d.getMinutes();
+    stime = d.getSeconds();
+    hrotation = 30*htime + mtime/2;
+    mrotation = 6*mtime;
+    srotation = 6*stime;
+
+    hour.style.transform = `rotate(${hrotation}deg)`;
+    minute.style.transform = `rotate(${mrotation}deg)`;
+    second.style.transform = `rotate(${srotation}deg)`;
+},1000)
+
+ setInterval(() => {
+    today = new Date();
+    var dd = String(today.getDate()).padStart(2, '0');
+    var mm = String(today.getMonth() + 1).padStart(2, '0');
+    var yyyy = today.getFullYear();
+
+    today = dd + '/' + mm + '/' + yyyy;
+    console.log(today);
+    document.getElementById('date').innerText = today;
+},1000) 
